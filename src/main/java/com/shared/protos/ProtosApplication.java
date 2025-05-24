b61346a6b6ba44a48e2ea3f1de2152f685b9d372
SidheshV <vaity.sidhesh.18it2008@gmail.com>
2025-05-24 18:38:41 +0530

    Setup proto repo

---
+++ src/main/java/com/shared/protos/ProtosApplication.java
@@ -0,0 +1,10 @@
+package com.shared.protos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtosApplication {
+    public static void main(String[] args) {
+        System.out.println("This is a shared-protos library project. Main class exists for Spring Boot plugin.");
+    }
+}
